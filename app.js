b54edaf520093b812ae9b2af5a3d1d205b2bb50d
Asta Rem <asta.rem@hotmail.com>
2025-02-12 18:28:59 +0200

    added validate Input - sanitize value

--- app.js
+++ app.js
@@ -8,17 +8,22 @@
 
 const inputForm = document.querySelector('[data-input-form]');
 const result = document.querySelector('[data-result]');
+const metricInput  = document.getElementById('metric-input');
+
+const metricChoice = inputForm.querySelector('#metric-unit').value;
+const imperialChoice = document.querySelector('#imperial-unit').value;    
+
+let amount = '';
+let validate;
+
 
 function showInputValues(event){
     event.preventDefault(); // stops the page refresh(before I can see the result in console)
-    const amount = inputForm.querySelector("input").value; //input is in the form, so it is better select it in the form scope, not in document
-    // const metricChoice = document.getElementById('metric-unit').value;
-    const metricChoice = inputForm.querySelector('#metric-unit').value;
-    const imperialChoice = document.querySelector('#imperial-unit').value;
-    console.log(imperialChoice);
-    console.log(metricChoice);
-    console.log(amount);
-    inputForm.querySelector("input").value = ''; // clears input field 
+    amount = inputForm.querySelector("input").value; 
+    amount = validateInput();
+    // console.log(imperialChoice);
+    // console.log(metricChoice);
+    // console.log(amount);
 
     let conversionKey = `${metricChoice}_to_${imperialChoice}`;
 
@@ -28,6 +33,7 @@ function showInputValues(event){
     console.log(output);
 
     result.innerText = output;
+    // console.log(`number entered: ${amount}`);
 }
 
 inputForm.addEventListener("submit", showInputValues);
@@ -46,4 +52,43 @@ const conversionFactors = {
     'kilometer_to_mile': 0.6214
 
 }
+
+function validateInput(){
+ 
+    let sanitizedValue = amount.replace(/[^0-9]/g, '');
+    metricInput.value = sanitizedValue;
+
+    if(sanitizedValue.length === 0){
+        setError('Only numbers please');
+        return null;
+    }
+
+    const number = parseInt(sanitizedValue, 10); // 10 - desimtaine sistema
+    console.log(`here is the number variable: ${number}`);
+    clearError();
+    return number;
+}
+
+function setError(message){
+    infoMsg.textContent = '';
+    infoMsg.style.opacity = '0';
+
+    errorMsg.textContent = message;
+    errorMsg.style.opacity = '1';
+    errorMsg.style = 'color: red;text-align: center;';
+    metricInput.style = 'border: 1px solid red ';
+    metricInput.value = '';
+}
+
+function clearError(){
+    errorMsg.textContent = '';
+    errorMsg.style.opacity = '0';
+    metricInput.style.border = 'none';
+    infoMsg.style.opacity = '1';
+
+}
+
+
+
+
 
